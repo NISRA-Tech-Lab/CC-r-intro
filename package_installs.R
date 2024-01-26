eacb5f3601b617dd87a3503f5181702b8894c6d7
kham1508 <107246450+kham1508@users.noreply.github.com>
2024-01-26 15:34:19 +0000

    setting package installs to binary only

--- package_installs.R
+++ package_installs.R
@@ -2,9 +2,9 @@
 # This package is part of the tidyverse, a set of packages created by RStudio
 # to allow users to work in a tidy manner
 # dplyr is the main data manipulation package - find out more here: https://www.tidyverse.org/
-install.packages("dplyr")
+install.packages("dplyr", type = "binary")
 # these are some other packages that we will need later
-install.packages("plotly")
-install.packages("ggplot2")
-install.packages("rmarkdown")
-install.packages("kableExtra")
+install.packages("plotly", type = "binary")
+install.packages("ggplot2", type = "binary")
+install.packages("rmarkdown", type = "binary")
+install.packages("kableExtra", type = "binary")
