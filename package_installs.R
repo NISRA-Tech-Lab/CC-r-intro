6b4dc0b7c80557065cfabd110de1a70feb428825
kham1508 <107246450+kham1508@users.noreply.github.com>
2024-10-21 11:53:31 +0100

    remove package installs script

--- package_installs.R
+++
@@ -1,8 +0,0 @@
-# To install a new package we run the command install.packages()
-# This package is part of the tidyverse, a set of packages created by RStudio
-# to allow users to work in a tidy manner
-# dplyr is the main data manipulation package - find out more here: https://www.tidyverse.org/
-install.packages("dplyr", type = "binary")
-# these are some other packages that we will need later
-install.packages("plotly", type = "binary")
-install.packages("ggplot2", type = "binary")
